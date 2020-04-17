76972ab2787970cc91351b874dd34bb3e180302a
Best_Lilys <2429715377@qq.com>
2020-04-17 23:23:07 +0800

    第三次作业第6题改正

--- countword.py
+++ countword.py
@@ -13,14 +13,17 @@ import os,re
 def readtext(filename):
     worddict={}
     with open (filename,'r',encoding='utf-8') as f:
-        txt=f.read()
+        txt=f.read(2000)
         txt=txt.lower()
-        wordlist=re.split(r'[}{\;?<>:|,./~!@$%^&*" \n()“”-]',txt)
+        wordlist=re.split(r'[}{\;?<>:|,./~! @$%^&*"\n()“”-]',txt)
         for word in wordlist:
-            if(word not in worddict):
+            if(word not in worddict and word!=''):
                 worddict[word]=wordlist.count(word)
     return worddict
 worddict=readtext('text.txt')
+wordlist=[]
 for word in worddict:
-    print("{}:{}".format(word,worddict[word]))
-
+    wordlist.append((word,worddict[word]))
+wordlist.sort(key=lambda x:x[1],reverse=True)
+for i in range(20):
+    print(wordlist[i][0],end='  ')
